6af8117ff7074f1f63762c5d8348645a12766cd3
IT21178740 <99183786+IT21178740@users.noreply.github.com>
2022-02-09 02:41:47 +0000

    i completed tutorial 1

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,17 @@
 #include <stdio.h>
 
 int main() {
-  
+  int mark1,mark2;
+  float average;
+
+  printf("Enter mark1:");
+  scanf("%d",&mark1);
+  printf("Enter mark2:");
+  scanf("%d",&mark2);
+
+  average=(mark1+mark2)/2;
+
+  printf("Average=%.2f\n",average);
   return 0;
 }
 
